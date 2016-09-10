7dc3276dc11c1ed152edf5b6e0a5ee7a597dea53
zmmx011 <zmmx019@gmail.com>
2016-09-10 14:08:45 +0900

    no message

---
+++ src/main/java/com/reizen/interceptor/SessionCheckInterceptor.java
@@ -0,0 +1,11 @@
+package com.reizen.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class SessionCheckInterceptor extends HandlerInterceptorAdapter {
+
+	public SessionCheckInterceptor() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
