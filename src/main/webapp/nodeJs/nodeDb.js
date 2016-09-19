9a592a097f34a9c11ff462d3816b68dd3da2924f
zmmx011 <zmmx019@gmail.com>
2016-09-19 20:13:46 +0900

    Merge branch 'ksh' of https://github.com/soohyunko/reizen into damu

---
+++ src/main/webapp/nodeJs/nodeDb.js
@@ -0,0 +1,6 @@
+exports.url = 'reizen-aws-db.c7a0sjzncliz.ap-northeast-2.rds.amazonaws.com';
+exports.username = 'reizen';
+exports.password = 'reizen1234';
+//exports.url = '192.168.0.28';
+//exports.username = 'reizen',
+//exports.password = '1234'	
